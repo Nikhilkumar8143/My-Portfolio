44697de2a076c56bd7c0754ffd79d3683976bd78
Nikhilkumar8143 <nikhilkumarthota42@gmail.com>
2020-08-03 21:27:06 +0530

    my portfolio first commit

---
+++ src/resumeData.js
@@ -0,0 +1,110 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Nikhil Kumar",
+    "role": "Full Stack Developer",
+    "linkedinId":"https://www.linkedin.com/in/nikhil-kumar-5518461a0/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I want to be a code ninja and like to use various parts of frontend development and like to learn about new technologies. ",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/nikhil-kumar-5518461a0/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Nikhilkumar8143",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"instagram",
+          "url":"https://www.instagram.com/nikhi_l_24/",
+          "className":"fa fa-instagram"
+        }
+      ],
+    "aboutme":"I am currently a final year student at The SCSVMV University and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently learning new technologies. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://github.com/Nikhilkumar8143",
+    "education":[
+      {
+        "UniversityName":"SCSVMV University",
+        "specialization":"Bachelor of Technology",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2021",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Narayna Junior College",
+        "specialization":"MPC",
+        "MonthOfPassing":"April",
+        "YearOfPassing":"2017",
+        "Achievements":"I have scored 97% in my final Interboard Examination."
+      },
+      {
+        "UniversityName":"SPS High School",
+        "specialization":"SSC",
+        "MonthOfPassing":"March",
+        "YearOfPassing":"2015",
+        "Achievements":"I have scored 90% in my final Interboard Examination."
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Freelance Or Internship",
+        "specialization":"Full Stack Web Application",
+        "MonthOfLeaving":"April",
+        "YearOfLeaving":"2020",
+        "Achievements":"I developed a creditscore web application which is used for bank employees or other users who wants to analyse the data for model approaches."
+      },
+      {
+        "CompanyName":"Freelance",
+        "specialization":"Interactive Website",
+        "MonthOfLeaving":"June",
+        "YearOfLeaving":"2020",
+        "Achievements":"I added extra features to an eSports web page email subscribes,tournament pages for hosting and to register"
+      }
+    ],
+    "skillsDescription":"My Programming Technologies ",
+    "skills":[
+      {
+        "skillname":"JavaScript",
+        "skilllevel":1290
+      },
+      {
+        "skillname":"Reactjs",
+        "skilllevel":90
+      },
+      {
+        "skillname":"Python-Flask",
+        "skilllevel":90
+      },
+      {
+        "skillname":"Mongo-DB",
+        "skilllevel":90
+      },
+      {
+        "skillname":"SQL",
+        "skilllevel":90
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"creditscore",
+        "description":"webapp",
+        "imgurl":"images/portfolio/creditscore.png"
+      },
+      
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
